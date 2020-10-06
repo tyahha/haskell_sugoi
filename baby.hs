ca0d9a3be270136a72ae7a943a9a398201b4ad65
toshinori sato <tyahhahha@gmail.com>
2020-10-06 23:35:33 +0900

    add sample function

--- baby.hs
+++ baby.hs
@@ -2,4 +2,16 @@ doubleMe :: Num a => a -> a
 doubleMe x = x + x
 
 doubleUs :: Num a => a -> a -> a
-doubleUs x y = x * 2 + y * 2
+doubleUs x y = doubleMe x + doubleMe y
+
+doubleSmallNumber :: (Ord a, Num a) => a -> a
+doubleSmallNumber x =
+  if x > 100
+    then x
+    else x * 2
+
+conanO'Brien :: String
+conanO'Brien = "It's a-me, Conan O'Brien"
+
+boomBangs :: Integral a => [a] -> [String]
+boomBangs xs = [if x < 10 then "BOOM!" else "BANG" | x <- xs, odd x]
