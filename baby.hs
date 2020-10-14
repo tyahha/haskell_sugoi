0da345933ecd807dae99fc15697b73c06b25bd47
toshinori sato <tyahhahha@gmail.com>
2020-10-14 23:49:29 +0900

    add some function for learing pattern match

--- baby.hs
+++ baby.hs
@@ -15,3 +15,25 @@ conanO'Brien = "It's a-me, Conan O'Brien"
 
 boomBangs :: Integral a => [a] -> [String]
 boomBangs xs = [if x < 10 then "BOOM!" else "BANG" | x <- xs, odd x]
+
+lucky :: Int -> String
+lucky 7 = "Lucky number seven!"
+lucky _ = "sorry, you're out of luck, pal!"
+
+factorial :: Int -> Int
+factorial 0 = 1
+factorial n = n * factorial (n - 1)
+
+charName :: Char -> String
+charName 'a' = "Albert"
+charName 'b' = "Broseph"
+charName 'c' = "Cecil"
+
+addVectors :: (Double, Double) -> (Double, Double) -> (Double, Double)
+addVectors (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
+
+first :: (a, b, c) -> addVectors
+first (x, _, _) = x
+
+second :: (a, b, c) -> c
+second (_, y, _) = y
