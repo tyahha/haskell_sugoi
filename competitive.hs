027a635b0b585f3ad496679047a2d501f6d6fd61
toshinori.sato <tyahhahha@gmail.com>
2025-09-17 22:10:38 +0900

    solve b03

--- competitive.hs
+++ competitive.hs
@@ -1,9 +1,17 @@
+combinations3 :: [a] -> [(a, a, a)]
+combinations3 xs =
+  [(x, y, z) |
+     (x, i) <- zip xs [0 .. ],
+     (y, j) <- zip xs [0 .. ],
+     i < j,
+     (z, k) <- zip xs [0 .. ],
+     j < k]
+
 main :: IO ()
 main = do
-  [_, n] <- map read . words <$> getLine :: IO [Int]
+  _ <- map read . words <$> getLine :: IO [Int]
   ps <- map read . words <$> getLine :: IO [Int]
-  qs <- map read . words <$> getLine :: IO [Int]
-  let found = any (\p -> any (\q -> p + q == n) qs ) ps
+  let found = any (\(x,y,z) -> x + y + z == 1000 ) (combinations3 ps)
   putStr $ if found
     then "Yes"
     else "No"
