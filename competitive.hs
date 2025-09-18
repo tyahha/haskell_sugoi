5469fd8b7ef9366cd98b7fefacc6b631338d5bea
toshinori.sato <tyahhahha@gmail.com>
2025-09-18 23:14:53 +0900

    solve b04

--- competitive.hs
+++ competitive.hs
@@ -1,12 +1,6 @@
-combinations3 :: Int -> String -> String
-combinations3 n s =
-  if n == 0
-    then s
-    else combinations3 (n `div` 2) ((if even n then '0' else '1') : s )
-
 main :: IO ()
 main = do
-  n <- readLn :: IO Int
-  let ss = combinations3 n ""
-  let rs = "0000000000" ++ ss
-  putStr $ drop (length rs - 10) rs
+  ss <- getLine
+  let rs = reverse ss
+  let n = foldl (\acc (c, e) -> acc + if c == '1' then e else 0) 0 (zip rs (iterate (*2) 1))
+  print n
