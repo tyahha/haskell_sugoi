33a63eb44aa5f65ab36ab494d2a2cfe0fb7ec3ee
toshinori.sato <tyahhahha@gmail.com>
2025-09-21 14:03:30 +0900

    improve performance

--- competitive.hs
+++ competitive.hs
@@ -1,8 +1,12 @@
+
+import Data.Array
+
 main :: IO ()
 main = do
-  [_, q] <- map read . words <$> getLine :: IO [Int]
+  [n, _] <- map read . words <$> getLine :: IO [Int]
   ps <- map read . words <$> getLine :: IO [Int]
-  let s = scanl (+) 0 ps
+  let sList = scanl (+) 0 ps
+      sArr = listArray (0, n) sList :: Array Int Int
   queries <- map (map read . words) . lines <$> getContents :: IO [[Int]]
-  let ans = map (\[l, r] -> s !! r - s !! (l - 1)) queries
+  let ans = map (\[l, r] -> sArr ! r - sArr ! (l - 1)) queries
   putStr $ unlines $ map show ans
